aa09d7cff8caf28a9b9dd957a2cc15a2a8d78c7d
Ruchit Raushan <ruchitraushan@gmail.com>
2024-09-15 00:50:04 +0530

    Server And Database Model Created

---
+++ backend/model/scorecard.py
@@ -0,0 +1,23 @@
+import enum
+from . import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Numeric 
+# import enum
+
+db = SQLAlchemy()
+class GenderEnum(enum.Enum):
+       male = 'Male'
+       femal = 'Female'
+       other = 'Other'
+
+class Students(db.Model):
+       id = db.Column(db.Integer, primary_key=True)
+       name = db.Column(db.String(100), nullable=False)
+       gender = db.Column(enum(GenderEnum), nullable=False)
+       maths_marks = db.Column(Numeric(precision=3, scale=2))
+       science_marks = db.Column(Numeric(precision=3, scale=2))
+       social_marks = db.Column(Numeric(precision=3, scale=2))
+
+
+       def __repr__(self):
+            return f'<Students {self.name}>'
